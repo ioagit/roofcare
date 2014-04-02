9ed140eacd21b998771e62fa9218e17e5e3089a8
isuarez <canadiense@gmail.com>
2014-04-02 18:44:07 -0400

    Testing Admin to access API/Users

--- test/config/routesSpec.js
+++ test/config/routesSpec.js
@@ -6,13 +6,29 @@ var config = require('../../server/config/config')[env];
 var server = require('../../server')(config);
 server.listen(config.port);
 
+var adminCredentials =  {
+    username: 'verita',
+    password: 'verita'
+};
+
+var contractorCredentials = {
+    username: 'rimita',
+    password: 'rimita'
+};
+var userCredentials = {
+    username: 'ioaioa',
+    password: 'ioaioa'
+};;
+
 
 describe ("Routes", function() {
 
 
     describe('/login', function() {
 
-        it('should return success with a valid username and password', loginUser());
+
+
+        it('should return success with a valid username and password', loginUser(adminCredentials));
 
         it('should return fail with an invalid username and password', loginInvalidUser());
 
@@ -20,7 +36,9 @@ describe ("Routes", function() {
 
     describe('/logout', function(done) {
 
-        it('should start with signin', loginUser());
+
+
+        it('should start with signin', loginUser(adminCredentials));
 
         it('should sign the user out', function(done) {
             request(server)
@@ -38,27 +56,55 @@ describe ("Routes", function() {
     });
 
 
-
-
     describe('GET /api/users', function(){
-        it('should response unauthorized status for non admin', function(done){
+
+        it('should response unauthorized status for anonymous', function(done){
             request(server)
                 .get('/api/users')
                 .expect(403, done);
         });
 
+        it('should start with signin non admin', loginUser(contractorCredentials));
+        it('should response unauthorized status for non admin', function(done){
+            request(server)
+                .get('/api/users')
+                .expect(403, done);
+        });
+        it('should sign the user out', function(done) {
+            request(server)
+                .get('/logout')
+                .expect(200, done);
+
+        });
+
+        it('should  signin an admin', loginUser(adminCredentials));
+        xit('should response with json for admin', function(done){
+            request(server)
+                .get('/api/users')
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(function(err, res){
+                    if (err) return done(err);
+
+                    done();
+                });
+        });
+        it('should log the user out', function(done) {
+            request(server)
+                .get('/logout')
+                .expect(200, done);
+
+        });
+
+
+
 
 
     });
 
-    function loginUser() {
+    function loginUser(credentials) {
 
         return function(done) {
-
-            var credentials = {
-                username: 'verita',
-                password: 'verita'
-            };
 
             request(server)
                 .post('/login')
@@ -99,7 +145,7 @@ describe ("Routes", function() {
 
             function onResponse(err, res) {
                 if (err) {
-                    return done(new Error(err.message));
+                    return done(err);
                 }
                 // this is should.js syntax, very clear
                 console.log(res.text)
