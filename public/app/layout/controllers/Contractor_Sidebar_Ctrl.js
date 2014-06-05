40dfd2c00cd3e266329cf241c6c0d1cd1f5fcdba
ioagit <canadiense@gmail.com>
2014-06-05 12:42:51 -0400

    saving a job. Not showing in the inbox list

---
+++ public/app/layout/controllers/Contractor_Sidebar_Ctrl.js
@@ -0,0 +1,29 @@
+/**
+ * Created by isuarez on 6/5/14.
+ */
+(function () {
+    'use strict';
+
+
+    //Handlers the Contractor SideBar Nav
+    var controllerId = 'ContractorSidebarCtrl';
+
+    angular.module('app').controller(controllerId,
+        ['$location', ContractorSidebarCtrl]);
+
+
+    function ContractorSidebarCtrl($location) {
+
+        var vm = this;
+
+        vm.isRouteCurrent = isRouteCurrent;
+
+
+
+        //Gets the current route
+        function isRouteCurrent(route) {
+            return $location.path().indexOf(route) > 0 ? 'active' : '';
+        }
+    }
+})();
+
